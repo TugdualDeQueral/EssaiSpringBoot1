faac7bc931a6409a161bb545f00a553ea5cddb5b
TugdualDeQueral <tugdual.pelledequeral@reseau.eseo.fr>
2022-03-21 15:49:28 +0100

    première sauvegarde

---
+++ EssaiSpringBoot1/src/main/java/com/example/EssaiSpringBoot1/EssaiSpringBoot1Application.java
@@ -0,0 +1,13 @@
+package com.example.EssaiSpringBoot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssaiSpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssaiSpringBoot1Application.class, args);
+	}
+
+}
---
+++ EssaiSpringBoot1/src/test/java/com/example/EssaiSpringBoot1/EssaiSpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EssaiSpringBoot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssaiSpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
